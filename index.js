438776d9213e51360b9b6fe5c1f9b3ba480cef10
804k <84988422+DesMS@users.noreply.github.com>
2021-11-26 21:06:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,51 +45,197 @@ module.exports = function(options = {
 				
 				if (options.forceCompression == false) {
 					if (enc.match(/\bgzip\b/)) {
-						res.writeHead(200, {
-							"content-encoding": "gzip"
-						});
-						raw.pipe(zlib.createGzip()).pipe(res);
-					} else if (enc.match(/\bdeflate\b/)) {
-						res.writeHead(200, {
-							"content-encoding": "deflate"
-						});
-						raw.pipe(zlib.createDeflate()).pipe(res);
-					} else if (enc.match(/\bbr\b/)) {
-						res.writeHead(200, {
-							"content-encoding": "br"
-						});
-						raw.pipe(zlib.createBrotliCompress()).pipe(res);
+const zlib = require(`zlib`);
+const fs = require(`fs`);
+
+module.exports = function(options = {
+	forceCompressionType: `Gzip`,
+	forceCompression: false,
+	changeSendFile: true,
+	changeSend: true,
+	cacheControl: `no-cache`,
+	flushInterval: 1500
+}) {
+	options = options || {};
+	if (options.forceCompressionType == undefined) {
+		options.forceCompressionType = `Gzip`;
+	}
+	if (options.forceCompression == undefined) {
+		options.forceCompression = false;
+	}
+	if (options.changeSendFile == undefined) {
+		options.changeSendFile = true;
+	}
+	if (options.changeSend == undefined) {
+		options.changeSend = true;
+	}
+	if (options.cacheControl == undefined) {
+		options.cacheControl = `no-cache`;
+	}
+	if (options.flushInterval == undefined) {
+		options.flushInterval = 1500;
+	}
+	return function(req, res, next) {
+		if (options.changeSendFile == true) {
+			res.sendFile = function(path, fn) {
+				try {
+					res.setHeader(`Cache-Control`, options[`cacheControl`]);
+					var enc = req.headers[`accept-encoding`];
+
+					if (!enc) {
+						enc = ``;
+					}
+				} catch (e) {
+					if (fn != undefined) {
+						fn(e);
 					} else {
-						res.writeHead(200, {});
-						raw.pipe(res);
+						console.error(e);
+					}
+					return;
+				}
+
+				var raw;
+				try {
+					fs.accessSync(path);
+					raw = fs.createReadStream(path);
+				} catch(e) {
+					if (fn != undefined) {
+						fn(e);
+					} else {
+						console.error(e);
+					}
+					return;
+				}
+				
+				if (options.forceCompression == false) {
+					if (enc.match(/\bgzip\b/)) {
+						try {
+							res.writeHead(200, {
+								"content-encoding": "gzip"
+							});
+							raw.pipe(zlib.createGzip()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
+					} else if (enc.match(/\bdeflate\b/)) {
+						try {
+							res.writeHead(200, {
+								"content-encoding": "deflate"
+							});
+							raw.pipe(zlib.createDeflate()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
+					} else if (enc.match(/\bbr\b/)) {
+						try {
+							res.writeHead(200, {
+								"content-encoding": "br"
+							});
+							raw.pipe(zlib.createBrotliCompress()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
+					} else {
+						try {
+							res.writeHead(200, {});
+							raw.pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
 					}
 				} else {
 					if (options.forceCompressionType.toLocaleLowerCase() == `gzip`) {
-						res.writeHead(200, {
-							"content-encoding": "gzip"
-						});
-						raw.pipe(zlib.createGzip()).pipe(res);
+						try {
+							res.writeHead(200, {
+								"content-encoding": "gzip"
+							});
+							raw.pipe(zlib.createGzip()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
 					} else if (options.forceCompressionType.toLocaleLowerCase() == `deflate`) {
-						res.writeHead(200, {
-							"content-encoding": "deflate"
-						});
+						try {
+							res.writeHead(200, {
+								"content-encoding": "deflate"
+							});
+							raw.pipe(zlib.createDeflate()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
 					} else if (options.forceCompressionType.toLocaleLowerCase() == `br` || options.forceCompressionType.toLocaleLowerCase() == `brotli`) {
-						res.writeHead(200, {
-							"content-encoding": "br"
-						});
-						raw.pipe(zlib.createBrotliCompress()).pipe(res);
+						try {
+							res.writeHead(200, {
+								"content-encoding": "br"
+							});
+							raw.pipe(zlib.createBrotliCompress()).pipe(res);
+						} catch (e) {
+							if (fn != undefined) {
+								fn(e);
+							} else {
+								console.error(e);
+							}
+							return;
+						}
 					} else {
-						console.error(`Invalid compression type.`);
+						if (fn != undefined) {
+							fn(e);
+						} else {
+							console.error(`Invalid compression type.`);
+						}
+						return;
 					}
 				}
 			}
 		}
+		if (options.changeSend == true) {
+			/*res.send = function(data) {
+
+			}*/
+		}
+		try {
 		var timer = setInterval(() => {
 			res.flush();
 		}, options.flushInterval);
+		} catch (e) {
+			console.error(e);
+		}
 
 		res.on(`close`, () => {
-			clearInterval(timer);
+			try {
+				clearInterval(timer);
+			} catch (e) {
+				console.error(e);
+			}
 		});
 		next();
 	}
